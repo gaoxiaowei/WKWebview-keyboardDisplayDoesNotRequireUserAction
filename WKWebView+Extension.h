b2c9de8ee86f196fe208b1e503a1a61e4b00229c
gaoxiaowei <gaoxiaowei@360.cn>
2022-03-15 16:11:35 +0800

    add files

---
+++ WKWebView+Extension.h
@@ -0,0 +1,17 @@
+//
+//  WKWebView+Extension.h
+//
+//  Created by gaoxiaowei on 2022/3/14.
+//
+
+#import <WebKit/WebKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WKWebView (Extension)
+
++(void)allowDisplayingKeyboardWithoutUserAction;
+
+@end
+
+NS_ASSUME_NONNULL_END
